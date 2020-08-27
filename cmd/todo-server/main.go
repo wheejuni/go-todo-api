439ec8d3860c45d812b29e792f0c564187283b69
hwijun-jeong <hwijun.jeong@myrealtrip.com>
2020-08-27 09:25:00 +0900

    8081 port로 기본 정보 노출해보기

--- cmd/todo-server/main.go
+++ cmd/todo-server/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "이것은 고랑 기반 서버이다. 당신의 요청 정보: %s", request.URL.Path)
+	})
+
+	http.ListenAndServe(":8081", nil)
 }
