93bebba4547c57419468622eee17c7266d9981a7
hwijun-jeong <hwijun.jeong@myrealtrip.com>
2020-08-27 09:34:30 +0900

    test static file 추가

--- cmd/todo-server/main.go
+++ cmd/todo-server/main.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("web/static/"))
+
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(writer, "이것은 고랑 기반 서버이다. 당신의 요청 정보: %s", request.URL.Path)
 	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":8081", nil)
 }
