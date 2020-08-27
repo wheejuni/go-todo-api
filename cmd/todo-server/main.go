555fa1487960c74fbee7a93e4cde343bc9def2e2
hwijun-jeong <hwijun.jeong@myrealtrip.com>
2020-08-27 09:43:44 +0900

    mux handler 적용

--- cmd/todo-server/main.go
+++ cmd/todo-server/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
+	router := mux.NewRouter()
 	fs := http.FileServer(http.Dir("web/static/"))
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(writer, "이것은 고랑 기반 서버이다. 당신의 요청 정보: %s", request.URL.Path)
 	})
 
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	router.HandleFunc("/hello/{name}", func(writer http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		fmt.Fprintf(writer, "%s야, 반갑다. 즐거운 코딩 생활 되길 바란다.", vars["name"])
+	})
 
-	http.ListenAndServe(":8081", nil)
+	router.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8081", router)
 }
